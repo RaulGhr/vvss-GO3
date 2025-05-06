e25bd52d15d8d6ef51c74551bf38a81d5e67d8fe
Raul Gherman <raul.gherman@rebeldot.com>
2025-05-06 12:22:13 +0300

    fix:wbt fix4

--- src/test/java/pizzashop/service/PizzaServiceTestGetTotalAmount.java
+++ src/test/java/pizzashop/service/PizzaServiceTestGetTotalAmount.java
@@ -12,12 +12,18 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-// @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PizzaServiceTestGetTotalAmount {
 
-    private PizzaService service = new PizzaService(null, null, null);
+    private PizzaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new PizzaService(null, null, null);
+    }
 
     @Test
+    @Order(1)
     void getTotalAmountTest1() {
         List<Payment> paymentList = new ArrayList<>();
         paymentList.add(new Payment(5, PaymentType.CASH, 13.0f));
@@ -26,6 +32,7 @@ class PizzaServiceTestGetTotalAmount {
     }
 
     @Test
+    @Order(2)
     void getTotalAmountTest2() {
         List<Payment> paymentList = new ArrayList<>();
         double result = service.getTotalAmount(paymentList, PaymentType.CASH);
@@ -33,12 +40,14 @@ class PizzaServiceTestGetTotalAmount {
     }
 
     @Test
+    @Order(3)
     void getTotalAmountTest3() {
         double result = service.getTotalAmount(null, PaymentType.CASH);
         assertEquals(0.0f, result);
     }
 
     @Test
+    @Order(4)
     void getTotalAmountTest4() {
         List<Payment> paymentList = new ArrayList<>();
         paymentList.add(new Payment(1, PaymentType.CARD, 12.0f));
@@ -49,6 +58,7 @@ class PizzaServiceTestGetTotalAmount {
     }
 
     @Test
+    @Order(5)
     void getTotalAmountTest5() {
         List<Payment> paymentList = new ArrayList<>();
         paymentList.add(new Payment(5, PaymentType.CASH, 13.0f));
@@ -57,6 +67,7 @@ class PizzaServiceTestGetTotalAmount {
     }
 
     @Test
+    @Order(6)
     void getTotalAmountTest6() {
         List<Payment> paymentList = new ArrayList<>();
         paymentList.add(new Payment(5, PaymentType.CASH, 13.0f));
